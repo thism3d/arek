e6ffb7e42e0198c0984e0c37a0e64f7ae9a6b7f0
Md Muzahidul Islam <muzahid@onzep.uk>
2024-09-25 15:56:48 +0600

    Admin Connectserver Fixed

--- admin/admin_server_files/connectserver.php
+++ admin/admin_server_files/connectserver.php
@@ -1,2 +1,17 @@
 <?php
-require '../server_files/connectserver.php';
+
+$servername = "localhost";
+$username = "moshcplo_arpm";
+$password = '3v.2ryGQ3jF9';
+$dbname = "moshcplo_arpm";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    echo "Database Communicatoin Error, Server in Maintainance, Call us immediately to solve this issue +447456289388.";
+    die("Connection failed: " . $conn->connect_error);
+    exit();
+}
+
