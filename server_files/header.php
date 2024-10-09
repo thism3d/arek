c0ff18a23316f2bb9c5205eb04dda592897e5de0
Md Muzahidul Islam <muzahid@onzep.uk>
2024-10-09 19:57:52 +0600

    Google Tag Manager

--- server_files/header.php
+++ server_files/header.php
@@ -39,6 +39,14 @@ $canonical = '<link rel="canonical" href="' . htmlspecialchars($currentUrl, ENT_
     gtag('config', 'G-VKLD2KTQ7L');
     </script>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TKFLP2C6');</script>
+
+
     <!-- PWA Code -->
     <link rel="manifest" href="manifest.json" />
     <meta name="theme-color" content="#000f1f">
@@ -69,6 +77,10 @@ $canonical = '<link rel="canonical" href="' . htmlspecialchars($currentUrl, ENT_
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKFLP2C6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <section class="upper_header">
         
